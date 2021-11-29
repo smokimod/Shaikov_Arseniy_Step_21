5fe1c4899155d16080af6e157714305ddecf25cb
smokimod <sagees@mai.ru>
2021-11-29 23:02:40 +0300

    123

--- React/newtodolist/src/App.jsx
+++ React/newtodolist/src/App.jsx
@@ -1,14 +1,14 @@
+import React from "react";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
-import {
-  faEdit,
-  faTimes,
-  faTrash,
-  faPluSquare,
-} from "@fortawesome/free-solid-svg-icons";
+import { faEdit, faTrash, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { List } from "./Components/List";
 
 export const App = () => {
-  library.add(fab, faEdit, faTrash, faTimes, faPluSquare);
-  return <List />;
+  library.add(fab, faEdit, faTrash, faTimes);
+  return (
+    <div>
+      <List />
+    </div>
+  );
 };
