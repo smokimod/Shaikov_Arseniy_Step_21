1c58b86167f36b830bdf6db1a0388398d847a745
smokimod <85346735+smokimod@users.noreply.github.com>
2021-11-30 18:08:37 +0200

    Delete React/react-homework-club directory

--- React/react-homework-club/src/Components/Achives.jsx
+++
@@ -1,8 +0,0 @@
-export function Ach(props) {
-  return (
-    <div>
-      <div>Общее число забиваний: {props.item.goals}</div>
-      <div>Лющее число побед: {props.item.wins}</div>
-    </div>
-  );
-}
